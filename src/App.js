03842aac001e639a4a64bc00f04c5ac03d5e23b2
Gaur <gvenika@mgc.mentorg.com>
2020-02-10 11:28:57 -0600

    Commit the local branch 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Pack lunch',
-        completed: false
+        completed: true
       },
       {
         id: 3,
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todo extends Component {
+
+
   render() {
     return this.props.todo.map((todolist) => (
-        <TodoItem />
+        <TodoItem key = {todolist.id} todo = {todolist} markComplete={this.markComplete}/>
     ))
   }
+}
+
+//PropTypes
+Todo.propTypes = {
+    todo: PropTypes.array.isRequired
 }
 
 export default Todo;
